48b747cdeab68d9b541d0fbb1701364fa06699c7
Flavius10 <flaviusa1010@gmail.com>
2025-12-05 12:16:13 +0200

    refactor Catalog
    refactor Catalog class adding the getAllItems meethod

--- src/main/java/com/Itemize/Itemize/domain/Catalog.java
+++ src/main/java/com/Itemize/Itemize/domain/Catalog.java
@@ -1,7 +1,11 @@
 package com.Itemize.Itemize.domain;
 
+import java.util.List;
+
 public interface Catalog {
     void addItem(Item item);
     void removeItem(Item item);
     Item getItemById(String id);
+
+    List<Item> getAllItems();
 }
