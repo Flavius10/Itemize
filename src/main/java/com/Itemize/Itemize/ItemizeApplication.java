b382bc2050a0ad32c31aa4fdd63a2142f31cb905
Flavius10 <flaviusa1010@gmail.com>
2025-12-05 10:06:45 +0200

    feat Itemize
    feat a new project for my personal portofolio

---
+++ src/main/java/com/Itemize/Itemize/ItemizeApplication.java
@@ -0,0 +1,13 @@
+package com.Itemize.Itemize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemizeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Itemize/Itemize/ItemizeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Itemize.Itemize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemizeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
